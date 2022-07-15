77beb1290ce3dc498e4d7babbe9dbc3c4e6aaf75
Tom Helmuth <thelmuth@hamilton.edu>
2022-07-15 14:16:27 -0400

    Updates to HPC launchers

--- mass_hpc_launcher.py
+++ mass_hpc_launcher.py
@@ -1,35 +1,41 @@
 import os
 
-name = "-HumanDS"
+name = "-HumanDS" ## Human Driven Simulation
 
-problems = [#"number-io",
-            #  "small-or-large",
-            #  "for-loop-index",
-            "compare-string-lengths",
-            "double-letters",
-            "fuel-cost",
-            "fizz-buzz",
-            #  "replace-space-with-newline",
-            #  "string-lengths-backwards",
-            #  "last-index-of-zero",
-            # "vector-average",
-            "count-odds",
-            "mirror-image",
-            "paired-digits",
-            "snow-day",
-            "gcd",
-            # "super-anagrams",
-            # "sum-of-squares",
-            "vectors-summed",
-            # "x-word-lines",
-            # "negative-to-zero",
-            "scrabble-score",
-            # "checksum",
-            # "digits",
-            # "grade",
-            # "median",
-            "smallest",
-            # "syllables"
+problems = [
+            ### PSB1
+            #"software.number-io",
+            #  "software.small-or-large",
+            #  "software.for-loop-index",
+            "software.last-index-of-zero",
+            "software.compare-string-lengths",
+            "software.double-letters",
+            "software.scrabble-score",
+            "software.count-odds",
+            #  "software.replace-space-with-newline",
+            #  "software.string-lengths-backwards",
+            # "software.vector-average",
+            # "software.mirror-image",
+            # "software.super-anagrams",
+            # "software.sum-of-squares",
+            # "software.vectors-summed",
+            # "software.x-word-lines",
+            # "software.negative-to-zero",
+            # "software.checksum",
+            # "software.digits",
+            # "software.grade",
+            # "software.median",
+            # "software.smallest",
+            # "software.syllables"
+
+            ### PSB2
+            "psb2.fuel-cost",
+            "psb2.fizz-buzz",
+            "psb2.gcd",
+            "psb2.paired-digits",
+            "psb2.snow-day",
+            "psb2.camel-case",
+            # "psb2.find-pair",
 ]
 
 with open('hpc_launcher.template', 'r') as hpc_template:
